90f20c1f1caa8f4471a3ad0f01136d616b6a6dd2
Andrew N. Sorkin <andrew.sorkin@octopus.com>
2022-11-28 17:17:09 -0500

    updated Azure-IaC-Terraform-create.tf

--- Azure-IaC-Terraform-create.tf
+++ Azure-IaC-Terraform-create.tf
@@ -54,69 +54,68 @@ resource "azurerm_storage_account" "asorkinsa1" {
 
 # SQL Server #
 
-  resource "azurerm_mssql_server" "asorkin_rq_mssql_server" {
-    name                         = "asorkin_rq_mssql_server"
-    resource_group_name             = "#{Project.Azure.RandomQuotes.ResourceGroup.Name}"
-    location                        = "#{Project.Azure.PrimaryLocation.Name}"
-    version                      = "12.0"
-    administrator_login          = "#{Project.Azure.RandomQuotes.DB.User}"
-    administrator_login_password = "#{Project.Azure.RandomQuotes.DB.Password}"
-    minimum_tls_version             = "1.2"
-    public_network_access_enabled   = true
-    tags = {
-      environment = "#{Octopus.Environment.Name}"    	
-    }
+resource "azurerm_mssql_server" "asorkin_rq_mssql_server" {
+  name                         = "asorkin-rq-mssql-server"
+  resource_group_name             = "#{Project.Azure.RandomQuotes.ResourceGroup.Name}"
+  location                        = "#{Project.Azure.PrimaryLocation.Name}"
+  version                      = "12.0"
+  administrator_login          = "#{Project.Azure.RandomQuotes.DB.User}"
+  administrator_login_password = "#{Project.Azure.RandomQuotes.DB.Password}"
+  minimum_tls_version             = "1.2"
+  public_network_access_enabled   = true
+  tags = {
+    environment = "#{Octopus.Environment.Name}"    	
   }
+}
 
 # SQL Database #
 
-  resource "azurerm_mssql_database" "asorkin_rq_database" {
-    name           = "#{Project.Azure.RandomQuotes.DB.Name}"
-    server_id      = azurerm_mssql_server.asorkin_rq_mssql_server.id
-    collation      = "SQL_Latin1_General_CP1_CI_AS"
-    license_type   = "LicenseIncluded"
-    max_size_gb    = 1
-    read_scale     = false
-    sku_name       = "S0"
-    zone_redundant = false
-    tags = {
-        environment = "#{Octopus.Environment.Name}"    	
-    }
+resource "azurerm_mssql_database" "asorkin_rq_database" {
+  depends_on = [ azurerm_mssql_server.asorkin_rq_mssql_server ]
+  name           = "#{Project.Azure.RandomQuotes.DB.Name}"
+  server_id      = azurerm_mssql_server.asorkin_rq_mssql_server.id
+  collation      = "SQL_Latin1_General_CP1_CI_AS"
+  license_type   = "LicenseIncluded"
+  max_size_gb    = 1
+  read_scale     = false
+  sku_name       = "S0"
+  zone_redundant = false
+  tags = {
+    environment = "#{Octopus.Environment.Name}"    	
   }
+}
 
 ###########################################
 # Azure Service Plan #
 ###########################################
 
-  resource "azurerm_service_plan" "asorkin_rq_sp" {
-    name                         = "asorkin_rq_sp"
-    resource_group_name             = "#{Project.Azure.RandomQuotes.ResourceGroup.Name}"
-    location                        = "#{Project.Azure.PrimaryLocation.Name}"
-    sku {
-      tier = "Standard"
-      size = "S1"
-    tags = {
-      environment = "#{Octopus.Environment.Name}"    	
-    }  
-  }
+resource "azurerm_service_plan" "asorkin_rq_sp" {
+  name                         = "asorkin-rq-sp"
+  resource_group_name          = "#{Project.Azure.RandomQuotes.ResourceGroup.Name}"
+  location                     = "#{Project.Azure.PrimaryLocation.Name}"
+  os_type                      = "Windows"
+  sku_name = "S1"
+  tags = {
+    environment = "#{Octopus.Environment.Name}"    	
+  }  
 }
 
 ###########################################
 # Azure Windows Web App #
 ###########################################
 
-  resource "azurerm_windows_web_app" "asorkin_rq_web_app" {
-    name                = "asorkin_rq_web_app"
-    resource_group_name             = "#{Project.Azure.RandomQuotes.ResourceGroup.Name}"
-    location                        = "#{Project.Azure.PrimaryLocation.Name}"
-    service_plan_id     = azurerm_app_service_plan.asorkin_rq_sp.id
-  
-    site_config {}
+resource "azurerm_windows_web_app" "asorkin_rq_web_app" {
+  name                = "asorkin-rq-web-app"
+  resource_group_name             = "#{Project.Azure.RandomQuotes.ResourceGroup.Name}"
+  location                        = "#{Project.Azure.PrimaryLocation.Name}"
+  service_plan_id     = azurerm_service_plan.asorkin_rq_sp.id
 
-    tags = {
-      octopus-environment = "#{Octopus.Environment.Name}"
-    	octopus-space = "#{Octopus.Space.Name}"
-    	octopus-role = "#{Project.Azure.RandomQuotes.Role}"
+  site_config {}
+
+  tags = {
+    octopus-environment = "#{Octopus.Environment.Name}"
+  	octopus-space = "#{Octopus.Space.Name}"
+  	octopus-role = "#{Project.Azure.RandomQuotes.Role}"
     }
   }
 
@@ -124,14 +123,14 @@ resource "azurerm_storage_account" "asorkinsa1" {
 # Azure Windows Web App Slot #
 ###########################################
 
- resource "azurerm_windows_web_app_slot" "staging" {
+resource "azurerm_windows_web_app_slot" "staging" {
   name           = "staging"
   app_service_id = azurerm_windows_web_app.asorkin_rq_web_app.id
 
   site_config {}
 }
 
- resource "azurerm_windows_web_app_slot" "production" {
+resource "azurerm_windows_web_app_slot" "production" {
   name           = "production"
   app_service_id = azurerm_windows_web_app.asorkin_rq_web_app.id
 
